3c458b32a29882a28bcebb7f852da93bff06feac
GANETHRAHESHAN <heshanpathirana1234@gmail.com>
2026-05-31 19:24:22 +0530

    Added new program

---
+++ HelloWolrd&MySelf/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("I am Ganethra Heshan Pathirana\n");
+    printf("My birthday is 2004-11-16\n");
+    printf("My hometown is Pasyala, Gampaha\n");
+    printf("I am studying at Faculty of Applied Science, University Of Sri Jayawardanapura\n");
+    printf("My subject combination is Mathematics, statistics, computer science\n");
+
+    return 0;
+}
